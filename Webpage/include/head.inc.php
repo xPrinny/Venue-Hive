fc6b2d6cadb1607b23618fcadc8e6aab711357aa
Seri <serihaniffah@gmail.com>
2023-03-10 02:04:31 +0800

    Update head.inc.php

--- Webpage/include/head.inc.php
+++ Webpage/include/head.inc.php
@@ -10,11 +10,12 @@
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="vendor/animate.css/animate.min.css">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendow/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="vendor/swiper/swiper-bundle.min.css">
 
     <!-- Custom  CSS File -->
     <link rel="stylesheet" href="css/main.css">
