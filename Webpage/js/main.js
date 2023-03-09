a47767a24efeb95792d47c4b8820a8b1b462fac7
Seri <serihaniffah@gmail.com>
2023-03-09 11:58:38 +0800

    PHP and all

--- Webpage/js/main.js
+++ Webpage/js/main.js
@@ -1,115 +1,206 @@
-/// Article slider
-// Get the slider container element
-const slider = document.querySelector('.slider');
-
-// Get all slider item elements
-const slides = document.querySelectorAll('.slide');
-
-// Set a variable to keep track of the current slide index
-let currentIndex = 0;
-
-// Create a function to move to the next slide
-function nextSlide() {
-// Hide the current slide
-    slides[currentIndex].classList.remove('active');
-
-// Increment the current index
-    currentIndex++;
-
-// If the current index is greater than the number of slides, reset it to 0
-    if (currentIndex >= slides.length) {
-        currentIndex = 0;
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
     }
-
-// Show the new current slide
-    slides[currentIndex].classList.add('active');
-
-}
-
-// Set an interval to automatically switch to the next slide every 5 seconds
-setInterval(nextSlide, 5000);
-
-// Latest news
-// Get the container element for the latest news section
-const latestNews = document.querySelector('.latest-news');
-
-// Get all news article elements
-const articles = document.querySelectorAll('.article');
-
-// Convert the articles NodeList to an array so we can use array methods
-const articlesArray = Array.from(articles);
-
-// Sort the articles array by date
-articlesArray.sort((a, b) => {
-    return new Date(b.dataset.date) - new Date(a.dataset.date);
-});
-
-// Loop through the sorted articles array and append each article to the latest news container
-articlesArray.forEach(article => {
-    latestNews.appendChild(article);
-});
-
-// News categories
-// Get the container element for the news categories section
-const newsCategories = document.querySelector('.news-categories');
-
-// Get all news article elements with a data-category attribute
-const categoryArticles = document.querySelectorAll('.article[data-category]');
-
-// Loop through each category article and create a button element for its category
-categoryArticles.forEach(article => {
-// Get the category of the current article
-    const category = article.dataset.category;
-
-// Check if a button element for this category already exists
-    const existingButton = newsCategories.querySelector(`button[data-category="${category}"]`);
-
-    if (existingButton) {
-        // If a button element already exists, append the article to its corresponding list
-        const categoryList = existingButton.nextElementSibling;
-        categoryList.appendChild(article);
-    } else {
-        // If a button element doesn't exist, create one and append it to the news categories container
-        const button = document.createElement('button');
-        button.textContent = category;
-        button.dataset.category = category;
-        newsCategories.appendChild(button);
-
-        // Create a new list element for this category and append the current article to it
-        const categoryList = document.createElement('ul');
-        categoryList.appendChild(article);
-        newsCategories.appendChild(categoryList);
-
-        // Add a click event listener to the button to show/hide the corresponding category list
-        button.addEventListener('click', () => {
-            categoryList.classList.toggle('active');
-        });
-    }
-
-});
-
-// Search bar
-// Get the search input element
-const searchInput = document.querySelector('#search');
-
-// Get all news article elements with a data-title attribute
-const titleArticles = document.querySelectorAll('.article[data-title]');
-
-// Add a keyup event listener to the search input
-searchInput.addEventListener('keyup', () => {
-// Get the search query from the input value
-    const searchQuery = searchInput.value.trim().toLowerCase();
-// Loop through each title article and check if its title contains the search query
-    titleArticles.forEach(article => {
-        const title = article.dataset.title.toLowerCase();
-        if (title.includes(searchQuery)) {
-// If the article title contains the search query, show it
-            article.classList.remove('hidden');
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      let selectEl = select(el, all)
+      if (selectEl) {
+        if (all) {
+          selectEl.forEach(e => e.addEventListener(type, listener))
         } else {
-
-// If the article title does not contain the search query, hide it by adding the 'hidden' class
-            article.classList.add('hidden');
+          selectEl.addEventListener(type, listener)
         }
+      }
+    }
+  
+    /**
+     * Easy on scroll event listener 
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener)
+    }
+  
+    /**
+     * Toggle .navbar-reduce
+     */
+    let selectHNavbar = select('.navbar-default')
+    if (selectHNavbar) {
+      onscroll(document, () => {
+        if (window.scrollY > 100) {
+          selectHNavbar.classList.add('navbar-reduce')
+          selectHNavbar.classList.remove('navbar-trans')
+        } else {
+          selectHNavbar.classList.remove('navbar-reduce')
+          selectHNavbar.classList.add('navbar-trans')
+        }
+      })
+    }
+  
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+          backtotop.classList.add('active')
+        } else {
+          backtotop.classList.remove('active')
+        }
+      }
+      window.addEventListener('load', toggleBacktotop)
+      onscroll(document, toggleBacktotop)
+    }
+  
+    /**
+     * Preloader
+     */
+    let preloader = select('#preloader');
+    if (preloader) {
+      window.addEventListener('load', () => {
+        preloader.remove()
+      });
+    }
+  
+    /**
+     * Search window open/close
+     */
+    let body = select('body');
+    on('click', '.navbar-toggle-box', function(e) {
+      e.preventDefault()
+      body.classList.add('box-collapse-open')
+      body.classList.remove('box-collapse-closed')
+    })
+  
+    on('click', '.close-box-collapse', function(e) {
+      e.preventDefault()
+      body.classList.remove('box-collapse-open')
+      body.classList.add('box-collapse-closed')
+    })
+  
+    /**
+     * Intro Carousel
+     */
+    new Swiper('.intro-carousel', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      }
     });
-});
-
+  
+    /**
+     * Property carousel
+     */
+    new Swiper('#property-carousel', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.propery-carousel-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+  
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
+    });
+  
+    /**
+     * News carousel
+     */
+    new Swiper('#news-carousel', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.news-carousel-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+  
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
+    });
+  
+    /**
+     * Testimonial carousel
+     */
+    new Swiper('#testimonial-carousel', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.testimonial-carousel-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    });
+  
+    /**
+     * Property Single carousel
+     */
+    new Swiper('#property-single-carousel', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.property-single-carousel-pagination',
+        type: 'bullets',
+        clickable: true
+      }
+    });
+  
+  })()
---
+++ Webpage/php/head.inc.php
@@ -0,0 +1,29 @@
+<meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>VenueHive Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+   
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    
