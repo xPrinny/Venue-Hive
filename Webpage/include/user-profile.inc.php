6f128652d4fe729d78ccce035767c0664d4366b9
xPrinny <blue_caocao@hotmail.com>
2023-03-12 04:19:44 +0800

    Reworked the homepage.

--- Webpage/include/user-profile.inc.php
+++
@@ -1,6 +0,0 @@
-<!-- do edit what is necessary-->
-
-<div class="profile-info">
-  <h2><?php echo $user['name']; ?></h2>
-  <p><?php echo $user['email']; ?></p>
-</div>
---
+++ Webpage/js/scripts.js
@@ -0,0 +1,43 @@
+/*!
+* Start Bootstrap - New Age v6.0.6 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+});
+
+// On load
+$(function() {
+    $('#carouselSideText').html($('.active > .carousel-review').html());
+});
+
+// Carousel side text
+$('.carousel').on('slid.bs.carousel', function () {
+    $('#carouselSideText').html($('.active > .carousel-review').html());
+});
