2ad3e47a79581453699dd933300b99d819966124
Seri <serihaniffah@gmail.com>
2023-03-25 23:55:11 +0800

    contact, privacy policy, faq, about me, still not done will edit after DSA
    there's issues with the formating, will do my best to resolve after dsa test

--- Webpage/footer.php
+++ Webpage/footer.php
@@ -3,11 +3,11 @@
     <div class="container p-4">
         <div class="text-white-50 small">
             <div class="mb-2">&copy; VenueHive. All Rights Reserved.</div>
-            <a href="#!">Privacy</a>
+            <a href="privacypolicy.php">Privacy Policy</a>
             <span class="mx-1">&middot;</span>
-            <a href="#!">Terms</a>
+            <a href="contactus.php">Contact Us</a>
             <span class="mx-1">&middot;</span>
-            <a href="#!">FAQ</a>
+            <a href="FAQ.php">FAQ</a>
         </div>
     </div>
 </footer>
