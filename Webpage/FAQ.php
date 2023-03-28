5f0bd7c49732ba1391ef328e70deeb84ed1da574
Seri <serihaniffah@gmail.com>
2023-03-29 00:51:38 +0800

    about us & faq with their css updated and include about us in footer

--- Webpage/FAQ.php
+++ Webpage/FAQ.php
@@ -2,89 +2,40 @@
 <html lang="en">
 
 <head>
-    <title>Venue Hive - FAQ</title>
-    <?php include "head.inc.php"; ?>
-    <!--<style>
-    body {
-      background-color: yellow;
-      color: black;
-      font-family: Arial, sans-serif;
-      font-size: 16px;
-      line-height: 1.5;
-      padding: 0;
-      margin: 0;
-    }
-
-    .container {
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 40px;
-      background-color: white;
-      box-shadow: 0 0 10px rgba(0,0,0,0.2);
-    }
-
-    h1 {
-      font-size: 36px;
-      text-align: center;
-      margin-bottom: 40px;
-    }
-
-    .question {
-      font-size: 24px;
-      font-weight: bold;
-      margin-bottom: 10px;
-    }
-
-    .answer {
-      margin-bottom: 30px;
-    }
-  </style>-->
+  <title>Venue Hive - FAQ</title>
+  <?php include "head.inc.php"; ?>
 </head>
 
 <body>
-    <!-- Navigation -->
-    <?php include "navbar.php"; ?>
-
-    <!-- Login Modal -->
-    <?php include "login.php"; ?>
-
-    <div class="container">
-        <h1>FAQ</h1>
-        <div class="question">
-            <p>What is VenueHive?</p>
+  <!-- Navigation -->
+  <?php include "navbar.php"; ?>
+  <!-- Login Modal -->
+  <?php include "login.php"; ?>
+  <main role="main">
+    <header class="masthead">
+      <div class="faq-container">
+        <h1>Frequently Asked Questions</h1>
+        <div class="faq-box" role="region" aria-labelledby="search-question">
+          <h2 id="search-question" class="faqcolor">How do I search for event spaces on VenueHive?</h2>
+          <p>To search for event spaces on VenueHive, simply enter your location and desired date range on the homepage search bar, and browse through the available options that meet your criteria.</p>
         </div>
-        <div class="answer">
-            <p>VenueHive is an event space rental page that helps people find and book event spaces for various occasions, such as weddings, birthdays, and corporate events.</p>
+        <div class="faq-box" role="region" aria-labelledby="booking-question">
+          <h2 id="booking-question" class="faqcolor">How do I book an event space on VenueHive?</h2>
+          <p>To book an event space on VenueHive, simply select the space you want, choose your desired date range and any additional services you need, and follow the prompts to complete your reservation and payment.</p>
         </div>
-
-        <div class="question">
-            <p>How do I search for event spaces on VenueHive?</p>
+        <div class="faq-box" role="region" aria-labelledby="cancel-question">
+          <h2 id="cancel-question" class="faqcolor">What if I need to cancel or reschedule my reservation?</h2>
+          <p>If you need to cancel or reschedule your reservation, please contact VenueHive support as soon as possible to make the necessary arrangements.</p>
         </div>
-        <div class="answer">
-            <p>To search for event spaces on VenueHive, simply enter your location and desired date range on the homepage search bar, and browse through the available options that meet your criteria.</p>
-        </div>
-
-        <div class="question">
-            <p>How do I book an event space on VenueHive?</p>
-        </div>
-        <div class="answer">
-            <p>To book an event space on VenueHive, simply select the space you want, choose your desired date range and any additional services you need, and follow the prompts to complete your reservation and payment.</p>
-        </div>
-
-        <div class="question">
-            <p>What if I need to cancel or reschedule my reservation?</p>
-        </div>
-        <div class="answer">
-            <p>If you need to cancel or reschedule your reservation, please contact VenueHive support as soon as possible to make the necessary arrangements.</p>
-        </div>
-
-    </div>
-    <!-- Footer -->
+      </div>
+    </header>
+    <!--Footer-->
     <?php include "footer.php"; ?>
     <!-- Bootstrap core JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS -->
     <script src="js/scripts.js"></script>
+  </main>
 </body>
 
 </html>
---
+++ Webpage/aboutus.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">>
+
+<head>
+    <title>About Venue Hive</title>
+    <?php include "head.inc.php" ?>
+</head>
+
+<body>
+    <main>
+        <!-- Navigation -->
+        <?php include "navbar.php"; ?>
+        <!-- Login Modal -->
+        <?php include "login.php"; ?>
+
+
+        <header class="masthead">
+            <section class="hero">
+                <div class="container">
+                    <h1>Who We Are</h1>
+                    <p>VenueHive is a one-stop-shop for finding and booking event venues. Our platform connects venue owners and event planners, making it easy to find the perfect space for any event. We pride ourselves on providing exceptional customer service and are committed to making the event planning process as seamless as possible.</p>
+                </div>
+            </section>
+
+            <section class="about">
+                <div class="container">
+                    <h2>What We Do</h2>
+                    <p>VenueHive provides users with a platform to find venues for their events, meetings, or gatherings. The site also offers venue owners an opportunity to earn an income by renting out their spaces to users.</p>
+                </div>
+            </section>
+
+            <section class="abtfeatured1">
+                <div class="container">
+                    <h2>Where We Operate</h2>
+                    <p>VenueHive operates online, making it accessible to users and venue owners from different locations.</p>
+                </div>
+            </section>
+
+            <section class="abtfeatured2">
+                <div class="container">
+                    <h2>When We're Available</h2>
+                    <p>VenueHive is available for use at any time, as long as the venues and venue owners are available to accommodate the users' needs.</p>
+                </div>
+            </section>
+
+            <section class="abtfeatured3">
+                <div class="container">
+                    <h2>Why We Exist</h2>
+                    <p>VenueHive was created to provide a convenient and accessible platform for users to find suitable venues for their events and for venue owners to earn extra income by renting out their spaces.</p>
+                </div>
+            </section>
+
+            <section class="abtfeatured4">
+                <div class="container">
+                    <h2>How We Work</h2>
+                    <p>Users can search for available venues on the VenueHive website, select a suitable space, and make a booking.</p>
+                </div>
+            </section>
+
+
+
+        </header>
+        <!--Footer-->
+        <?php include "footer.php"; ?>
+        <!-- Bootstrap core JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS -->
+        <script src="js/scripts.js"></script>
+    </main>
+</body>
+</html>
