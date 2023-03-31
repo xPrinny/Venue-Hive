0c3fe1a9cee1958bec05c765bd1196763799ebbd
sherlynhao <122261185+sherlynhao@users.noreply.github.com>
2023-03-31 23:19:33 +0800

    show only received reviews

--- Webpage/utils/getReviews.php
+++ Webpage/utils/getReviews.php
@@ -3,7 +3,7 @@
     // Prepare statement that gets item information
    $stmt = $conn->prepare("SELECT a.memberId, ratingStar, ratingComment, a.username as 'reviewOwner', b.username as 'recievedUser', c.listingName FROM reviews
            INNER JOIN members a ON reviewOwnerId = a.memberId INNER JOIN members b ON reviewRecieveId = b.memberId INNER JOIN listings c ON reviewListId = c.listingId
-           WHERE a.username = '" . $username . "' OR b.username = '" . $username . "'");
+           WHERE b.username = '" . $username . "'");
     $stmt->execute();
     $result = $stmt->get_result();
     if ($result->num_rows > 0) {
